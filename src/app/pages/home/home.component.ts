a0d44651c18772c457f246877217b6a4d9deb92e
maatheusfp <matheus.pinheirofer@gmail.com>
2024-07-16 15:48:34 -0300

    home page done

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  private router = inject(Router);
+
+  goLogin(){
+    this.router.navigate(['login']);
+  }
+
+  goSignUp(){
+    this.router.navigate(['signup']);
+  }
+}
